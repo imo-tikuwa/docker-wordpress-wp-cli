5adf07f4d9da66505cf4d3bbcdc437734ac0208f
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-09-21 19:00:20 +0900

    All in one wp migrationのリストア用の記述を削除

--- docker/wp/wp-install.sh
+++ docker/wp/wp-install.sh
@@ -2,8 +2,8 @@
 
 set -ex;
 
-# WP_INSTALL_PATHがサブディレクトリの場合はホストPCと共有してるディレクトリを削除しようとしてエラーが出る
-rm -fR /var/www/html/*
+# WP_INSTALL_PATHがサブディレクトリの場合はホストPCと共有してるディレクトリを削除しようとしてエラーが出る。
+rm -fR /var/www/html/* || true
 
 mkdir -p $WP_INSTALL_PATH
 wp --allow-root core download --version=latest --locale=ja --path=$WP_INSTALL_PATH --force
@@ -12,9 +12,3 @@ wp --allow-root core install --title=$WP_BLOG_TITLE --admin_user=$WP_ADMIN_USER 
 wp --allow-root plugin uninstall akismet hello --path=$WP_INSTALL_PATH
 wp --allow-root plugin install wp-multibyte-patch --activate --path=$WP_INSTALL_PATH
 wp --allow-root plugin install all-in-one-wp-migration --activate --path=$WP_INSTALL_PATH
-
-# wp-cliからのall-in-one-wp-migrationを介したリストアは有償のエクステンションが必要だった模様^^;
-# if [ -f /tmp/restore.wpress ] then
-#     mv /tmp/restore.wpress $WP_INSTALL_PATH/wp-content/ai1wm-backups
-#     wp --allow-root ai1wm restore restore.wpress
-# fi
